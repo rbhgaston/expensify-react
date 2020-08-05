23118242cda496dae9dbaaa707466af0658a9918
rbhgaston <rbhga@rbhgaston-pc.local>
2020-08-05 21:57:56 +0100

    remove generated files by webpack from public

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports= (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: "bundle.js"
         },
         module: {
@@ -58,6 +58,7 @@ module.exports= (env) => {
         // cheap-module-eval-source-map
         devtool: isProd ? 'source-map' : 'inline-source-map',
         devServer: {
+            publicPath: '/dist/',
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true
         }
